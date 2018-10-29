c8450ee7eef3c7dd92726e574f2a478cabc58251
Todd McGrath <mcgrath@supergloo.com>
2018-10-29 11:26:59 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # cassandra-ingest
 WIP
+
+# Overview
+There are many ways to implement RDBMS to Cassandra Ingest using StreamSets.  We're going to go through a few examples in this
+repo.  We'll cover two patterns: 1) Initial historical bulk load and 2) Change Data Capture (CDC) trickle feeds
+
+In these examples, we're going to riff off the infamous Killrvideo app and database.  In these examples, we'll present and solve for the use case where we built a site called Killrmovies in an RDBMS but we want to move to Cassandra for various reasons.  How could we do that?
+
+//TODO Notes
+
+* After bulk load which may have a ton of tombstones?  Whadda do
+
+* Maybe add more content like preview_image, location, etc.
+
+* Maybe add Redis or caching lookup before using JDBC Lookup
+
+* Maybe add `cdc.type` field to each table (or have that has a part 2).  Need to research appropriate cassandra data model for this
+
+
+// Requirements
+Cassandra, mySQL (any JDBC db such as Oracle, MS SQL, Postgres), StreamSets Data Collector
+
+// Setup notes
+Cassandra running
+Start cassandra `bin/cassandra -f`
+bin/cqlsh -f ~/dev/cassandra-ingest/schema/cassandra_schema.cql
+
+Add mySQL steps
+
+-- select t.*, r.* from tags t cross join ratings r on t.user_id = r.user_id where t.user_id = 1
+
+// Maybe Dockerize this ^ 
+
+* RDMBS to Cassandra Bulk Load
+
+Note about auto-increment doesn't work well with distributed db like cassandra so create mapping tables in source db for bulk load lookups, so we implemented a GUID mapping lookup in the database.
+
+
---
+++ schema/killrmovies_mysql_schema.sql
@@ -0,0 +1,116 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.20-log)
+# Database: killrmovies
+# Generation Time: 2018-10-29 16:15:42 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table _movie_guid
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `_movie_guid`;
+
+CREATE TABLE `_movie_guid` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `movie_id` int(11) NOT NULL,
+  `guid` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table _user_guid
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `_user_guid`;
+
+CREATE TABLE `_user_guid` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `guid` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table movies
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table ratings
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `ratings`;
+
+CREATE TABLE `ratings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `rating` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table tags
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tags`;
+
+CREATE TABLE `tags` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `tag` varchar(100) DEFAULT '',
+  `user_id` int(11) DEFAULT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
